bfb7ff8c06adf8522b86ce825cd8245a117b64f7
tianhaobaoq <127838303+tianhaobaoq@users.noreply.github.com>
2026-07-28 03:22:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
 const photos=[];
 
-// Cambia este número por la cantidad de fotos que tengas
-const TOTAL=20;
+const TOTAL=20; // cambia según tus fotos
+
 
 for(let i=1;i<=TOTAL;i++){
+
     photos.push(`fotos/${i}.jpg`);
+
 }
 
-const img=document.getElementById("photo");
 
 let index=0;
 
+const img=document.getElementById("photo");
+
+
+
+function startAlbum(){
+
+
+    document.getElementById("popup").style.display="none";
+
+
+    changePhoto();
+
+
+    setInterval(changePhoto,5000);
+
+
+}
+
+
+
 function changePhoto(){
+
 
     img.classList.remove("show");
 
+
     setTimeout(()=>{
 
+
         img.src=photos[index];
+
 
         img.onload=()=>{
 
             img.classList.add("show");
 
-        }
+        };
+
 
         index++;
 
+
         if(index>=photos.length){
+
             index=0;
+
         }
+
 
     },700);
 
+
 }
-
-changePhoto();
-
-setInterval(changePhoto,5000);
